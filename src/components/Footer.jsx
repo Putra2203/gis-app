4745f65fcac60c47802838f7e839d021efc8cacb
Putra2203 <erdin22xii@gmail.com>
2024-09-27 23:16:28 +0700

    all clear

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import logo from "../Asset/footer-icon.png";
 import map from "../Asset/map.png";
+import { IoMdCall } from "react-icons/io";
+import { IoMail, IoLocation } from "react-icons/io5";
 
 const Footer = () => {
   return (
@@ -27,15 +29,17 @@ const Footer = () => {
 
         <nav className="flex flex-col gap-4">
           <h6 className="underline footer-title">Kontak Kami</h6>
-          <a className="link link-hover hover:text-slate-200">08112681112</a>
-          <a className="link link-hover hover:text-slate-200">
-            Call Center Darurat 112
+          <a className="flex items-center gap-2 link link-hover hover:text-slate-200">
+            <IoMdCall /> 08112681112
           </a>
-          <a className="link link-hover hover:text-slate-200">
-            infomudik@semarangkota.go.id
+          <a className="flex items-center gap-2 link link-hover hover:text-slate-200">
+            <IoMdCall /> Call Center Darurat 112
           </a>
-          <a className="link link-hover hover:text-slate-200">
-            Jl. Pemuda No.148, Kota Semarang
+          <a className="flex items-center gap-2 link link-hover hover:text-slate-200">
+            <IoMail /> infomudik@semarangkota.go.id
+          </a>
+          <a className="flex items-center gap-2 link link-hover hover:text-slate-200">
+            <IoLocation /> Jl. Pemuda No.148, Kota Semarang
           </a>
         </nav>
 
@@ -47,7 +51,7 @@ const Footer = () => {
           >
             Lokasi
           </a>
-          <img src={map} alt="" className="mt-4"/>
+          <img src={map} alt="" className="mt-4" />
         </aside>
       </div>
     </footer>
--- src/components/TableK.jsx
+++ src/components/TableK.jsx
@@ -79,9 +79,12 @@ const MainComp = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const visiblePages = [];
-  const pageLimit = 5;  
+  const pageLimit = 5;
   let startPage = Math.max(currentPage - Math.floor(pageLimit / 2), 1);
-  let endPage = Math.min(startPage + pageLimit - 1, Math.ceil(filteredData.length / itemsPerPage));
+  let endPage = Math.min(
+    startPage + pageLimit - 1,
+    Math.ceil(filteredData.length / itemsPerPage)
+  );
 
   if (endPage - startPage < pageLimit - 1) {
     startPage = Math.max(endPage - pageLimit + 1, 1);
@@ -100,131 +103,144 @@ const MainComp = () => {
   }
 
   return (
-    <div className="container max-w-screen-lg p-10 mx-auto my-32 text-sm rounded-2xl bg-slate-200">
-      {/* Search Input and Button */}
+    <div className="flex max-w-screen-sm p-4 mx-auto my-32 lg:max-w-screen-lg md:max-w-screen-md">
+      <div className="container p-4 mx-auto text-sm lg:p-10 rounded-2xl bg-slate-200 ">
+        {/* Search Input and Button */}
 
-      <div className="flex justify-between">
-        <div >
-          <label htmlFor="itemsPerPage" className="mr-2 text-slate-900">
-            Items per page:
-          </label>
-          <select 
-            id="itemsPerPage"
-            value={itemsPerPage}
-            onChange={(e) => setItemsPerPage(Number(e.target.value))}
-            className="px-2 py-1 text-black bg-white border rounded"
-          >
-            <option value={5}>5</option>
-            <option value={10}>10</option>
-            <option value={15}>15</option>
-          </select>
-        </div>
-        
-        <div className="flex w-2/3 gap-4">
-          <button
-            onClick={handleSearch}
-            className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
-          >
-            Search
-          </button>
-          <input
-            type="text"
-            placeholder="Search by Kecamatan or Kelurahan"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value.toUpperCase())} // Automatically convert to uppercase
-            className="w-full px-4 py-2 bg-white border rounded-lg"
-          />
-        </div>
-      </div>
+        <div className="flex flex-col justify-between lg:flex-row">
+          <div className="flex mb-4">
+            <label htmlFor="itemsPerPage" className="mr-2 text-slate-900">
+              Show Entries :
+            </label>
+            <select
+              id="itemsPerPage"
+              value={itemsPerPage}
+              onChange={(e) => setItemsPerPage(Number(e.target.value))}
+              className="px-2 py-1 text-black bg-white border rounded"
+            >
+              <option value={5}>5</option>
+              <option value={10}>10</option>
+              <option value={15}>15</option>
+            </select>
+          </div>
 
-      <div className="grid items-center grid-cols-8 gap-4 p-4 mt-4">
-        <span className="col-span-1 text-gray-600">No</span>
-        <span className="col-span-2 text-gray-600">Kecamatan</span>
-        <span className="col-span-2 text-gray-600">Kelurahan</span>
-        <span className="col-span-1 text-gray-600">Distance</span>
-        <span className="col-span-1 text-gray-600">Map Link</span>
-        <span className="col-span-1 text-gray-600">Photo</span>
-      </div>
-
-      {currentItems.map((kelurahan, index) => (
-        <div
-          key={kelurahan.id}
-          className="grid items-center grid-cols-8 gap-4 p-4 mt-4 bg-gray-100 rounded-lg"
-        >
-          <div className="col-span-1">
-            <span className="text-gray-700">
-              {index + 1 + indexOfFirstItem}
-            </span>
-          </div>
-          <div className="col-span-2">
-            <span className="text-gray-700">{kelurahan.nama_kecamatan}</span>
-          </div>
-          <div className="col-span-2">
-            <span className="text-gray-900 ">{kelurahan.nama_kelurahan}</span>
-          </div>
-          <div className="col-span-1">
-            {userPosition.lat && userPosition.lon ? (
-              <span className="text-gray-700">
-                {haversineDistance(
-                  userPosition.lat,
-                  userPosition.lon,
-                  kelurahan.latitude,
-                  kelurahan.longitude
-                ).toFixed(2)}{" "}
-                km
-              </span>
-            ) : (
-              <span className="text-gray-700">Location unknown</span>
-            )}
-          </div>
-          <div className="col-span-1">
-            <a
-              href={`https://www.google.com/maps?q=${kelurahan.latitude},${kelurahan.longitude}`}
-              target="_blank"
-              rel="noopener noreferrer"
+          <div className="flex gap-4 lg:w-2/3">
+            <button
+              onClick={handleSearch}
               className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
             >
-              Buka Maps
-            </a>
-          </div>
-          <div className="col-span-1">
-            <img
-              src={`http://localhost:5000${kelurahan.gambar}`}
-              alt={kelurahan.nama_kelurahan}
-              className="object-cover w-32 h-24 rounded-lg"
+              Search
+            </button>
+            <input
+              type="text"
+              placeholder="Search by Kecamatan or Kelurahan"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value.toUpperCase())} // Automatically convert to uppercase
+              className="w-full px-4 py-2 bg-white border rounded-lg"
             />
           </div>
         </div>
-      ))}
 
-      {/* Pagination */}
-      <div className="flex justify-center mt-6 join no">
-        <button
-          className="text-black bg-white border-none hover:bg-slate-200 join-item btn"
-          onClick={() => paginate(Math.max(1, currentPage - 1))}
-          disabled={currentPage === 1}
-        >
-          «
-        </button>
-        <button className="text-black bg-white border-none hover:bg-slate-200 join-item btn">
-          Page {currentPage} of {Math.ceil(filteredData.length / itemsPerPage)}
-        </button>
-        <button
-          className="text-black bg-white border-none hover:bg-slate-200 join-item btn"
-          onClick={() =>
-            paginate(
-              Math.min(
-                currentPage + 1,
-                Math.ceil(filteredData.length / itemsPerPage)
+        <div className="items-center hidden gap-4 p-4 mt-4 md:grid lg:grid-cols-8 md:grid-cols-6">
+          <span className="text-gray-600 lg:col-span-1 md:col-span-1">No</span>
+          <span className="text-gray-600 lg:col-span-2 md:col-span-1">
+            Kecamatan
+          </span>
+          <span className="text-gray-600 lg:col-span-2 md:col-span-1">
+            Kelurahan
+          </span>
+          <span className="text-gray-600 lg:col-span-1 md:col-span-1">
+            Distance
+          </span>
+          <span className="text-gray-600 lg:col-span-1 md:col-span-1">
+            Map Link
+          </span>
+          <span className="text-gray-600 lg:col-span-1 md:col-span-1">
+            Photo
+          </span>
+        </div>
+
+        {currentItems.map((kelurahan, index) => (
+          <div
+            key={kelurahan.id}
+            className="flex flex-col items-center justify-center gap-4 p-4 mt-4 bg-gray-100 rounded-lg lg:grid lg:grid-cols-8 md:grid-cols-6"
+          >
+            <div className="lg:col-span-1 md:col-span-1">
+              <span className="text-gray-700">
+                {index + 1 + indexOfFirstItem}
+              </span>
+            </div>
+            <div className="lg:col-span-2 md:col-span-1">
+              <span className="text-gray-700">{kelurahan.nama_kecamatan}</span>
+            </div>
+            <div className="lg:col-span-2 md:col-span-1">
+              <span className="text-gray-900 ">{kelurahan.nama_kelurahan}</span>
+            </div>
+            <div className="lg:col-span-1 md:col-span-1">
+              {userPosition.lat && userPosition.lon ? (
+                <span className="text-gray-700">
+                  {haversineDistance(
+                    userPosition.lat,
+                    userPosition.lon,
+                    kelurahan.latitude,
+                    kelurahan.longitude
+                  ).toFixed(2)}{" "}
+                  km
+                </span>
+              ) : (
+                <span className="text-gray-700">Location unknown</span>
+              )}
+            </div>
+            <div className="items-center lg:col-span-1 md:col-span-1">
+              <a
+                href={`https://www.google.com/maps?q=${kelurahan.latitude},${kelurahan.longitude}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-4 py-2 text-white bg-blue-500 rounded-md lg:py-1 lg:px-2 md:px-1 md:py-1 hover:bg-blue-600"
+              >
+                Buka Maps
+              </a>
+            </div>
+            <div className="lg:col-span-1 md:col-span-1">
+              <img
+                src={`http://localhost:5000${kelurahan.gambar}`}
+                alt={kelurahan.nama_kelurahan}
+                className="object-cover rounded-lg lg:h-24 lg:w-32"
+              />
+            </div>
+          </div>
+        ))}
+
+        {/* Pagination */}
+        <div className="flex justify-center mt-6 join no">
+          <button
+            className="text-black bg-white border-none hover:bg-slate-200 join-item btn"
+            onClick={() => paginate(Math.max(1, currentPage - 1))}
+            disabled={currentPage === 1}
+          >
+            «
+          </button>
+          <button className="text-black bg-white border-none hover:bg-slate-200 join-item btn">
+            Page {currentPage} of{" "}
+            {Math.ceil(filteredData.length / itemsPerPage)}
+          </button>
+          <button
+            className="text-black bg-white border-none hover:bg-slate-200 join-item btn"
+            onClick={() =>
+              paginate(
+                Math.min(
+                  currentPage + 1,
+                  Math.ceil(filteredData.length / itemsPerPage)
+                )
               )
-            )
-          }
-          disabled={
-            currentPage === Math.ceil(filteredData.length / itemsPerPage)
-          }
-        >
-          »
-        </button>
+            }
+            disabled={
+              currentPage === Math.ceil(filteredData.length / itemsPerPage)
+            }
+          >
+            »
+          </button>
+        </div>
       </div>
     </div>
   );
